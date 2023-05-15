ed10e4fbbd7ca143fbdb796a6c79874f8629eee5
Rachid Benkitou <rachidbenkitou2021@gmail.com>
2023-05-15 18:35:05 +0100

    update

--- Eureka-Service/src/main/java/com/example/eureka_dis/EurekaDisApplication.java
+++ Eureka-Service/src/main/java/com/example/eureka_dis/EurekaDisApplication.java
@@ -1,18 +1,11 @@
 package com.example.eureka_dis;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
-@ EnableEurekaServer
+@EnableEurekaServer
 public class EurekaDisApplication {
 
 	public static void main(String[] args) {
